a0eafb67c04ae03db216da85d534bd069e274ca6
reddevilcero <reddevil_cero@hotmail.com>
2020-02-04 11:53:32 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+require_relative './user.rb'
+class Student < User
+  attr_accessor :knowledge
+  def initialize
+    self.knowledge = []
+  end
+
+  def learn(matter)
+    self.knowledge << matter
+  end
 
 end
